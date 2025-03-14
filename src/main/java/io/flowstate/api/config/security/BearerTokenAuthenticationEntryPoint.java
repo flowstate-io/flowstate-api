bd2cd94527f4bf1987b9796b3bb06d6da7b19eee
Your Name <sadlier.brendan15@gmail.com>
2025-03-14 15:08:13 +0000

    Config classes implemented

---
+++ src/main/java/io/flowstate/api/config/security/BearerTokenAuthenticationEntryPoint.java
@@ -0,0 +1,31 @@
+package io.flowstate.api.config.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class BearerTokenAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper;
+
+    public void commence(final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException authException) throws IOException {
+        final var status = HttpStatus.UNAUTHORIZED;
+
+        log.info("{}: {}", status.getReasonPhrase(), authException.getMessage());
+
+        response.setStatus(status.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        objectMapper.writeValue(response.getWriter(), forStatus(status).withErrorType(UNAUTHORIZED).build());
+    }
+}
